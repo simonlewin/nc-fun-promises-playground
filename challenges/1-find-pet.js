384c5349d3ccae917473845ac5e53d8718d9345b
jimstev2447 <jim.stevenson2447@gmail.com>
2023-11-09 14:49:46 +0000

    remove implementation

--- challenges/1-find-pet.js
+++ challenges/1-find-pet.js
@@ -1,14 +1,6 @@
-const fs = require("fs/promises");
+
 
 function findPet(petName) {
-  return fs
-    .readFile(`./data/${petName}.json`)
-    .then((data) => {
-      return JSON.parse(data);
-    })
-    .catch(() => {
-      return `soz couldnt find ${petName} :(`;
-    });
-}
+
 
 module.exports = findPet;
