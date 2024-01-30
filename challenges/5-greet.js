c10269e21a8a9a3b9761a6b59be92b6d429a401c
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 13:32:59 +0000

    feat: use inquirer to ask for a name and log a greeting
    install inquirer and use .prompt to log 'Hello <name>!'

---
+++ challenges/5-greet.js
@@ -0,0 +1,11 @@
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      name: "greeting",
+      message: "What is your name?",
+      type: "input",
+    },
+  ])
+  .then((answer) => console.log(`Hello ${answer.greeting}!`));
