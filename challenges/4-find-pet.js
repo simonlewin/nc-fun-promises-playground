d5abdf53d2293d3ce663cf560148c94879b500e2
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 13:30:14 +0000

    feat: add findPet fn
    add findPet to use fs/promises library to read pet data from a file given a pet name

--- challenges/4-find-pet.js
+++ challenges/4-find-pet.js
@@ -1,5 +1,10 @@
 const fs = require("fs/promises");
 
-const findPet = () => {};
+const findPet = (name) => {
+  return fs
+    .readFile(`./data/${name}.json`, "utf8")
+    .then((data) => JSON.parse(data))
+    .catch((error) => `soz couldnt find ${name} :(`);
+};
 
 module.exports = findPet;
