8d771f64411fba385141ed6927d02e225c3b488e
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 18:57:32 +0000

    feat: add 8-compare-and-combine
    add file to use fs/promises readFile method to read the contents of two .txt files and log the name of the one with the most characters plus the number of chars it contains, then use writeFile to merge the two files

---
+++ challenges/8-compare-combine.js
@@ -0,0 +1,20 @@
+const fs = require("fs/promises");
+
+const string1 = fs.readFile("./secret-message.txt", "utf8");
+const string2 = fs.readFile("./super-secret-message.txt", "utf8");
+
+Promise.all([string1, string2]).then((values) => {
+  const str1 = values[0];
+  const str2 = values[1];
+
+  const fileName =
+    str1.length > str2.length
+      ? "secret-message.txt"
+      : "super-secret-message.txt";
+
+  const lengthDifference = Math.abs(str1.length - str2.length);
+
+  console.log(`${fileName}`, lengthDifference);
+
+  fs.writeFile("./mega-secret-message.txt", str1 + "\n" + str2);
+});
