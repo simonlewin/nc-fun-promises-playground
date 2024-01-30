11534f4aea3aac95373ea4e54bccc682b6f11fd3
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 18:06:58 +0000

    fix: refactor findPet
    refactor findPet fn to use arrow functions and implict returns

--- challenges/4-find-pet.js
+++ challenges/4-find-pet.js
@@ -1,10 +1,9 @@
 const fs = require("fs/promises");
 
-const findPet = (name) => {
-  return fs
+const findPet = (name) =>
+  fs
     .readFile(`./data/${name}.json`, "utf8")
     .then((data) => JSON.parse(data))
     .catch((error) => `soz couldnt find ${name} :(`);
-};
 
 module.exports = findPet;
