922a395ba60d44bdd5e918a74415c304b6ca2ae3
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 13:27:27 +0000

    feat: add fetchAllCats fn
    add fetchAllCats fn to use of both fetchAllOwners and fetchCatsByOwner in order to retrieve an array of all the cats

--- challenges/3-fetch-all-cats.js
+++ challenges/3-fetch-all-cats.js
@@ -1,6 +1,12 @@
 const fetchCatsByOwner = require("./1-fetch-cats-by-owner");
 const fetchAllOwners = require("./2-fetch-all-owners");
 
-const fetchAllCats = () => {};
+const fetchAllCats = () => {
+  return fetchAllOwners().then((owners) => {
+    return Promise.all(owners.map(fetchCatsByOwner)).then((result) =>
+      result.flat().sort()
+    );
+  });
+};
 
 module.exports = fetchAllCats;
