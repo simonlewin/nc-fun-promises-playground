b76a34c15a9a088751907735b4d3155dd83f8329
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-01-30 18:03:53 +0000

    fix: refactor fetchAllCats
    refactor function to use arrow functions and implict returns

--- challenges/3-fetch-all-cats.js
+++ challenges/3-fetch-all-cats.js
@@ -1,12 +1,11 @@
 const fetchCatsByOwner = require("./1-fetch-cats-by-owner");
 const fetchAllOwners = require("./2-fetch-all-owners");
 
-const fetchAllCats = () => {
-  return fetchAllOwners().then((owners) => {
-    return Promise.all(owners.map(fetchCatsByOwner)).then((result) =>
+const fetchAllCats = () =>
+  fetchAllOwners().then((owners) =>
+    Promise.all(owners.map(fetchCatsByOwner)).then((result) =>
       result.flat().sort()
-    );
-  });
-};
+    )
+  );
 
 module.exports = fetchAllCats;
